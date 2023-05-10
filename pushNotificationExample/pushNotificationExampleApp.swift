88d54aa23af82447ad2d40641b6173f44b809bfb
XinLvlv <xinhaoyan@outlook.com>
2023-05-10 15:45:37 -0700

    Initial Commit

---
+++ pushNotificationExample/pushNotificationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  pushNotificationExampleApp.swift
+//  pushNotificationExample
+//
+//  Created by lvlv xin on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct pushNotificationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
